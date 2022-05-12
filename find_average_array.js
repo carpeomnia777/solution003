bed3f29e6a011e8926933697438b7e1234eaf895
Chris W <41281925+carpeomnia777@users.noreply.github.com>
2022-05-11 21:38:33 -0400

    calulate average completed -learn

---
+++ find_average_array.js
@@ -0,0 +1,11 @@
+// write a function that calculates the average of numbers in a given list
+function find_average(array) {
+    if (array.length === 0) {
+    return 0;
+    }
+    var result = 0;
+    for (i=0; i<array.length; i++) {
+      result +=array[i];
+    }
+    return result/array.length;
+  }
